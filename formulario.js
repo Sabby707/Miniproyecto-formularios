c7cc386ec076fffaa451d17e042c074b373e4db7
Sabby <marandersonsantillan@crackthecode.la>
2025-09-18 10:15:40 -0500

    modal y actualice enlaces

--- formulario.js
+++ formulario.js
@@ -31,21 +31,6 @@ document.getElementById("usuario").addEventListener("submit", function (event) {
     enviar(); 
 });
 
-function mostrar() {
-    const modal = document.getElementById("modal");
-    modal.style.display = "flex";
-
-    document.getElementById("cerrar").onclick = function() {
-        modal.style.display = "none";
-    };
-
-   
-    window.onclick = function(e) {
-        if (e.target === modal) {
-            modal.style.display = "none";
-        }
-    };
-}
 
 
 
